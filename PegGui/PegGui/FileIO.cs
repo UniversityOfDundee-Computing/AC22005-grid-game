3466a525402005e0de0603f2b89bc59f22f7cc74
Adam Mathieson <adam@genav.ch>
2022-01-25 15:38:26 +0000

    + Base function definitions for FileIO  + Peg base class  + Some Test bmps

--- PegGui/PegGui/FileIO.cs
+++ PegGui/PegGui/FileIO.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace PegGui
 {
     public static class FileIO
     {
-        public static Board ReadBoardFile()
+        public static Board ReadBoardFile(String name)
         {
             return new Board();
+        }
+
+        public static void WriteBoardFile(Board b, String name)
+        {
+
+        }
+
+        public static List<Tuple<string, int>> getHighscores(String name)
+        {
+            return new List<Tuple<string, int>>();
+        }
+
+        public static void addScore(int score, String playerName, String mapName)
+        {
+
         }
     }
 
--- PegGui/PegGui/Peg.cs
+++ PegGui/PegGui/Peg.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace PegGui
 {
+    /**
+     * Class that represents a single peg, this stores the state of a peg as well as it's coordinates.
+     */
     public class Peg
     {
+        Coord coors { get; set; }
+        enum Peg_State { EMPTY, SET, AVAILABLE, WALL };
+        Peg_State state { get; set; }
     }
 }
