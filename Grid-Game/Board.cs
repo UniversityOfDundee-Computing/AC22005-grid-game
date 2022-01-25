4d00dca8557bc3aab6b0b8d8d61ff8efe4a37632
Adam Mathieson <adam@genav.ch>
2022-01-25 13:57:37 +0000

    + Create Board, Coord, and FileIO Classes

---
+++ Grid-Game/Board.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+public class Board
+{
+	int _Height = 9;
+	int _Width = 9;
+	List<Coord[2]> history = new List<Coord[2]>;
+
+	public Board()
+	{
+	}
+}
---
+++ Grid-Game/FileIO.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+public class FileIO
+{
+	public FileIO()
+	{
+	}
+}
