89b94b5948972adaa2114eca9841fa2d23e15e37
Adam Mathieson <adam@genav.ch>
2022-02-02 10:29:23 +0000

    + Add generic ship placing logic

--- PegGui/PegGui/Board.cs
+++ PegGui/PegGui/Board.cs
@@ -8,13 +8,11 @@ namespace BattleShipGame
 		List<Tuple<Coord, Coord>> history = new List<Tuple<Coord, Coord>> { };
 
 		// 10x10 Board object
-		public Peg[,] board_Player = new Peg[10, 10];
-		public Peg[,] board_PlayerHits = new Peg[10, 10];
-		public Peg[,] board_Other = new Peg[10, 10];
-		public Peg[,] board_OtherHits = new Peg[10, 10];
+		public GridPosition[,] board_Player = new GridPosition[10, 10];
+		public GridPosition[,] board_Other = new GridPosition[10, 10];
 
-		public Ship[] playerShips = new Ship[10];
-		public Ship[] otherShips = new Ship[10];
+		public List<Ship> playerShips = new List<Ship>();
+		public List<Ship> otherShips = new List<Ship>();
 
 		public enum ACTION_STATE {
 			ACTION_SUCCESS,
@@ -28,9 +26,62 @@ namespace BattleShipGame
 		{
 		}
 
-		public ACTION_STATE PlacePlayerShip(Ship.SHIP_TYPE ship, int _X, int _Y, Ship.DIRECTION dir)
+		public ACTION_STATE PlaceShip(Ship.SHIP_TYPE ship, int _X, int _Y, Ship.DIRECTION dir, bool isPlayer)
         {
+			GridPosition[,] board_Tmp;
+			if (isPlayer)
+            {
+				board_Tmp = board_Player;
 
+				playerShips.Add(new Ship(ship, dir, _X, _Y));
+
+			} else
+            {
+				board_Tmp = board_Other;
+
+				otherShips.Add(new Ship(ship, dir, _X, _Y));
+			}
+			int xOff = 0;
+			int yOff = 0;
+			for (int i = 0; i < Ship.GetShipDimensions(ship); i++)
+            {
+				if ((_X + xOff>=0 && _X+xOff < board_Player.GetLength(1)) && (_Y + yOff >= 0 && _Y + yOff < board_Player.GetLength(0))  && board_Tmp[_X+xOff,_Y+yOff].ShipIndex != -1)
+                {
+					if (isPlayer)
+					{
+						playerShips.Remove(new Ship(ship, dir, _X, _Y));
+					}
+					else
+					{
+						otherShips.Remove(new Ship(ship, dir, _X, _Y));
+					}
+					return ACTION_STATE.ACTION_FAIL;
+                }
+
+				switch (dir)
+                {
+					case Ship.DIRECTION.UP:
+						yOff--;
+						break;
+					case Ship.DIRECTION.DOWN:
+						yOff++;
+						break;
+					case Ship.DIRECTION.LEFT:
+						xOff--;
+						break;
+					case Ship.DIRECTION.RIGHT:
+						xOff++;
+						break;
+                }
+            }
+
+			if (isPlayer)
+            {
+				board_Player = board_Tmp;
+            } else
+            {
+				board_Other = board_Tmp;
+            }
 			return ACTION_STATE.ACTION_SUCCESS;
         }
 
--- PegGui/PegGui/GridPosition.cs
+++ PegGui/PegGui/GridPosition.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PegGui
+namespace BattleShipGame
 {
     public class GridPosition
     {
