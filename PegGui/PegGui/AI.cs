7e74f9f68b3d6e2ef4b6cf7a40dd7abebe098f44
caydey <caydey@protonmail.com>
2022-02-08 22:23:41 +0000

    changed csproj to compile ai.cs and added other stuff

--- PegGui/PegGui/AI.cs
+++ PegGui/PegGui/AI.cs
@@ -1,16 +1,31 @@
 ﻿using System;
+using System.Collections.Generic;
+
+
 
 namespace BattleShipGame
 {
 	public class AI
 	{
-		public AI()
+		private Board board;
+		private DIFFICUILTY difficuilty;
+		private List<Coord> shots = new List<Coord>(); 
+		public AI(Board board, DIFFICUILTY difficuilty)
 		{
+			this.board = board;
+			this.difficuilty = difficuilty;
 		}
+		public enum DIFFICUILTY
+        {
+			EASY,
+			MEDIUM,
+			HARD,
+			IMPOSSIBLE
+        }
 
-		public void setShips(Board board)
+		public bool setShips()
 		{
-			cosnt bool IS_PLAYER = false;
+			const bool IS_PLAYER = false;
 			Random random = new Random();
 			// for each ship
 			for (int i = 0; i < board.INITIAL_SHIPS.Length; i++)
@@ -20,21 +35,35 @@ namespace BattleShipGame
 				// get random direction
 				Ship.DIRECTION randDir = (Ship.DIRECTION)random.Next(0, 4);
 				bool placed = false;
-				// get random coords try to place, if not fits, try again
+				// get random coords, try to place, if not fits, try again
 				while (!placed)
                 {
-					int randX = random.Next(0, 9);
-					int randY = random.Next(0, 9);
-					board.ACTION_STATE res = board.PlaceShip(ship_type, randX, randY, randDir, IS_PLAYER);
-					if (res == board.ACTION_STATE.SUCCESS)
+					int randX = random.Next(0, 8);
+					int randY = random.Next(0, 8);
+					Board.ACTION_STATE res = board.PlaceShip(ship_type, randX, randY, randDir, IS_PLAYER);
+					if (res == Board.ACTION_STATE.ACTION_SUCCESS)
                     {
 						placed = true;
                     }
                 }
 
             }
+			return true;
 
 		}
+
+		public bool getMove()
+        {
+			if (difficuilty == DIFFICUILTY.IMPOSSIBLE)
+            {
+
+            } else
+            {
+				///
+            }
+			return false;
+
+        }
 
 	}
 
