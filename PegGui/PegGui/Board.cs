f37be962de70843cdcbb883112025c291f13621e
Adam Mathieson <adam@genav.ch>
2022-02-09 10:51:50 +0000

    + Comments on Board class

--- PegGui/PegGui/Board.cs
+++ PegGui/PegGui/Board.cs
@@ -5,13 +5,20 @@ namespace BattleShipGame
 {
 	public class Board
 	{ 
-		// 10x10 Board object¦¦¦¦¦¦¦¦¦¦¦¦
+		// 10x10 Board object
+
+		// Global Properties for storing current state
 		public GridPosition[,] board_Player;
 		public GridPosition[,] board_Other;
 
 		public List<Ship> playerShips = new List<Ship>();
 		public List<Ship> otherShips = new List<Ship>();
 
+		// Counters to help with scoring
+		private int playerMoves { get; set; }
+		private int otherMoves { get; set; }
+
+		// Enums allowing for more legible code and allowing callers to easily know return state
 		public enum ACTION_STATE {
 			ACTION_SUCCESS,
 			ACTION_FAIL,
@@ -20,6 +27,7 @@ namespace BattleShipGame
 			ACTION_SHIP_SUNK
 		};
 
+		// Default ships available to a player in the game
 		public Ship.SHIP_TYPE[] INITIAL_SHIPS =
 		{
 			Ship.SHIP_TYPE.Patrol_Boat,
@@ -35,7 +43,7 @@ namespace BattleShipGame
 		};
 
 
-
+		// Main Constructor, setting up the GridPositions and initialising them correctly
 		public Board()
 		{
 			board_Player = new GridPosition[10, 10];
@@ -51,83 +59,94 @@ namespace BattleShipGame
 			}
 		}
 
+		// PlacePeg method, allows user (ai or player) to place a peg on the board and returns if it was a hit, a miss, or a fail (if a peg was already present there)
 		public ACTION_STATE PlacePeg(int _X, int _Y, bool isPlayer)
         {
-			if (!isPlayer)
-            {
-				if (board_Player[_X, _Y].hit)
-					return ACTION_STATE.ACTION_FAIL;
-
-				if (board_Player[_X,_Y].ShipIndex != -1)
-                {
-					board_Player[_X, _Y].hit = true;
-					playerShips[board_Player[_X, _Y].ShipIndex].Hits++;
-					if (playerShips[board_Player[_X, _Y].ShipIndex].Hits >= playerShips[board_Player[_X, _Y].ShipIndex].Length)
-						return ACTION_STATE.ACTION_SHIP_SUNK;
-
-					return ACTION_STATE.ACTION_HIT;
-				}
-            } else
-            {
-				if (board_Other[_X, _Y].hit)
-					return ACTION_STATE.ACTION_FAIL;
-
-				if (board_Other[_X, _Y].ShipIndex != -1)
-				{
-					board_Other[_X, _Y].hit = true;
-					otherShips[board_Other[_X, _Y].ShipIndex].Hits++;
-					if (otherShips[board_Other[_X, _Y].ShipIndex].Hits >= otherShips[board_Other[_X, _Y].ShipIndex].Length)
-						return ACTION_STATE.ACTION_SHIP_SUNK;
-
-					return ACTION_STATE.ACTION_HIT;
-				}
+			GridPosition[,] current_grid;
+			List<Ship> current_ship_list;
+			// Allocate correct current grid and list - helps remove duplicated code
+			if (!isPlayer) {
+				current_grid = board_Player;
+				current_ship_list = playerShips;
+			} else
+			{
+				current_grid = board_Other;
+				current_ship_list = otherShips;
 			}
+
+			// If the cell is already hit - fail
+			if (current_grid[_X, _Y].hit)
+				return ACTION_STATE.ACTION_FAIL;
+
+			// If the cell contains a ship - hit
+			if (current_grid[_X, _Y].ShipIndex != -1)
+			{
+				if (isPlayer)
+					this.playerMoves++;
+				else
+					this.otherMoves++;
+
+				current_grid[_X, _Y].hit = true;
+				current_ship_list[current_grid[_X, _Y].ShipIndex].Hits++;
+				// Check if ship is sunk - sunk
+				if (current_ship_list[current_grid[_X, _Y].ShipIndex].Hits >= current_ship_list[current_grid[_X, _Y].ShipIndex].Length)
+					return ACTION_STATE.ACTION_SHIP_SUNK;
+
+				return ACTION_STATE.ACTION_HIT;
+			}
+			
+			// Else - miss
 			return ACTION_STATE.ACTION_MISS;
         }
 
 
+		// Method validating and placing ships on the appropriate board
 		public ACTION_STATE PlaceShip(Ship.SHIP_TYPE ship, int _X, int _Y, Ship.DIRECTION dir, bool isPlayer)
         {
-			GridPosition[,] board_Tmp;
+			GridPosition[,] board_Tmp; // Reusable temp board
+			List<Ship> ships; // Reusable ships list
+			Ship shipObject = new Ship(ship, dir, _X, _Y); // New Ship object
+			
+			// Variables used for incrementing directions
+			int xOff = 0;
+			int yOff = 0;
+
+			// Set the temp board and add the ship to the relavent arrays (this is not TS but given this app is single threaded its fine)
 			if (isPlayer)
             {
-				board_Tmp = board_Player;
-
-				playerShips.Add(new Ship(ship, dir, _X, _Y));
+				board_Tmp = (GridPosition[,])board_Player.Clone();
+				ships = playerShips;
 
 			} else
             {
-				board_Tmp = board_Other;
-
-				otherShips.Add(new Ship(ship, dir, _X, _Y));
+				board_Tmp = (GridPosition[,])board_Other.Clone();
+				ships = otherShips;
 			}
-			int xOff = 0;
-			int yOff = 0;
-			for (int i = 0; i < Ship.GetShipDimensions(ship); i++)
-            {
 
-				if ((_X + xOff>=0 && _X+xOff < board_Player.GetLength(1)) && (_Y + yOff >= 0 && _Y + yOff < board_Player.GetLength(0))  && board_Tmp[_X+xOff,_Y+yOff].ShipIndex != -1)
+			ships.Add(shipObject);
+
+			// Itterate over length of the selected ship in the correct direction checking the poition is valid, if invalid remove it from the list and return fail
+			for (int i = 0; i < Ship.GetShipDimensions(ship); i++)
+			{
+
+				// Bounds checking and collision checking for a placed ship
+				if (
+					(_X + xOff >= 0 && _X + xOff < board_Player.GetLength(0)) && 
+					(_Y + yOff >= 0 && _Y + yOff < board_Player.GetLength(1)) && 
+					(board_Tmp[_X+xOff,_Y+yOff].ShipIndex != -1)
+					)
                 {
-					if (isPlayer)
-					{
-						playerShips.Remove(new Ship(ship, dir, _X, _Y));
-					}
-					else
-					{
-						otherShips.Remove(new Ship(ship, dir, _X, _Y));
-					}
+					// Remove the ship from the list and fail if collision with edge or other ship
+					ships.Remove(shipObject);
+
 					return ACTION_STATE.ACTION_FAIL;
                 }
 
-				if (isPlayer)
-				{
-					board_Tmp[_X + xOff, _Y + yOff].ShipIndex = playerShips.Count - 1;
-				}
-				else
-				{
-					board_Tmp[_X + xOff, _Y + yOff].ShipIndex = otherShips.Count - 1;
-				}
+				// Set the relavent cells to contain the appropriate ship index
+				board_Tmp[_X + xOff, _Y + yOff].ShipIndex = ships.Count - 1;
 
+
+				// based on ship direction increment the appropriate axis
 				switch (dir)
                 {
 					case Ship.DIRECTION.UP:
@@ -145,14 +164,35 @@ namespace BattleShipGame
                 }
             }
 
+			// Update the gamewide boards
 			if (isPlayer)
             {
-				board_Player = board_Tmp;
+				board_Player = (GridPosition[,])board_Tmp.Clone();
             } else
             {
-				board_Other = board_Tmp;
+				board_Other = (GridPosition[,])board_Tmp.Clone();
             }
 			return ACTION_STATE.ACTION_SUCCESS;
+        }
+
+		// Get number of ships still to sink
+		public int GetShipsToSink(bool isPlayer)
+        {
+			List<Ship> shipsList; // reusable ships list
+			int counter = 0; // Ship Counter
+			
+			// Set the reusable list
+			if (isPlayer)
+				shipsList = playerShips;
+			else
+				shipsList = otherShips;
+
+			// Itterate over still floating ships
+			foreach (Ship s in shipsList)
+				if (s.Hits < s.Length)
+					counter++;
+
+			return counter;
         }
 
 	}
