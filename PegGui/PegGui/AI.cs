6d576be4b493c73a1afa4f16d51a6b5de290e018
caydey <caydey@protonmail.com>
2022-02-10 12:16:44 +0000

    made x/y public

--- PegGui/PegGui/AI.cs
+++ PegGui/PegGui/AI.cs
@@ -5,17 +5,23 @@ using System.Collections.Generic;
 
 namespace BattleShipGame
 {
+	
+
 	public class AI
 	{
+		private bool foundAShip = false;
+		private FoundShip foundShip;
 		private Random random;
 		private Board board;
 		private DIFFICUILTY difficuilty;
+
 		public AI(Board board, DIFFICUILTY difficuilty)
 		{
 			this.board = board;
 			this.difficuilty = difficuilty;
 			this.random = new Random(); // so a new random object isnt created every time i need a random number
 		}
+
 		public enum DIFFICUILTY
         {
 			EASY,
@@ -52,6 +58,24 @@ namespace BattleShipGame
 
 		}
 
+		private Board.ACTION_STATE placeRandomPeg()
+        {
+			bool IS_PLAYER = false;
+			bool pegPlaced = false;
+			Board.ACTION_STATE state = Board.ACTION_STATE.ACTION_FAIL;
+			while (!pegPlaced)
+            {
+				int randX = random.Next(0, Board.SIZE);
+				int randY = random.Next(0, Board.SIZE);
+				state = board.PlacePeg(randX, randY, IS_PLAYER);
+				if (state != Board.ACTION_STATE.ACTION_FAIL) // not placed ontop of ship
+                {
+					pegPlaced = true;
+                }
+            }
+			return state;
+        }
+
 		public bool doMove()
         {
 			const bool IS_PLAYER = false;
@@ -77,27 +101,69 @@ namespace BattleShipGame
                 }
             } else if (difficuilty == DIFFICUILTY.EASY) // random hits
             {
-				bool pegPlaced = false;
-				while (!pegPlaced)
-                {
-					int randX = random.Next(0, Board.SIZE);
-					int randY = random.Next(0, Board.SIZE);
-					if (board.PlacePeg(randX, randY, IS_PLAYER) != Board.ACTION_STATE.ACTION_FAIL)
-                    {
-						pegPlaced = true;
-                    }
-                }
-				return true;
+				placeRandomPeg();
             } else if (difficuilty == DIFFICUILTY.MEDIUM) // random hits with trailing
 			{
-
+				
+				if (foundAShip) // just hit a ship
+                {
+					if (foundShip.foundDirection)
+                    {
+						Coord lastHit = foundShip.hits[foundShip.hits.Count -1];
+						Coord direction = foundShip.direction;
+						int nextX = lastHit.x + direction.x;
+						int nextY = lastHit.y + direction.y;
+						if (!(nextX >= 0 && nextY >= 0 && nextX < Board.SIZE && nextY < Board.SIZE))
+                        { // hit edge of board, flip direction and hit that side
+							foundShip.reverseDirection();
+							nextX = lastHit.x + foundShip.direction.x;
+							nextY = lastHit.y + foundShip.direction.y;
+                        }
+						Board.ACTION_STATE state = board.PlacePeg(nextX, nextY, IS_PLAYER);
+						if (state == Board.ACTION_STATE.ACTION_HIT)
+                        {
+							foundShip.hits.Add(new Coord(nextX, nextY));
+                        } else if (state == Board.ACTION_STATE.ACTION_MISS) // reached end of ship
+                        {
+							foundShip.reverseDirection();
+                        } else if (state == Board.ACTION_STATE.ACTION_SHIP_SUNK)
+                        {
+							foundAShip = false;
+							foundShip = null;
+                        }
+                    }            
+                } else // random shot
+                {
+					Board.ACTION_STATE state = placeRandomPeg();
+					if (state == Board.ACTION_STATE.ACTION_HIT)
+                    {
+						foundAShip = true;
+                    }
+                }
             }
-			return false;
-
+			return true;
         }
+		private class FoundShip
+		{
+			public List<Coord> hits;
+			public Coord direction;
+			public bool foundDirection;
+			public bool hitEnd; // to flip direction 4[3][2][1][5][6]  -  [] is ship N is hits
+			public FoundShip(int x, int y)
+			{
+				hits = new List<Coord>();
+				hits.Add(new Coord(x, y));
+				hitEnd = false;
+				foundDirection = false;
+			}
+			public void reverseDirection()
+            {
+				direction.x *= -1;
+				direction.y *= -1;
+            }
+		}
 
 	}
 
-	
 }
 
