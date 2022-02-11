da6374e6a3f280b2841759908d5817e57f75c0bc
EliotSott0 <elliotscott02@gmail.com>
2022-02-11 15:19:53 +0000

    new diffuculty

--- PegGui/PegGui/Startup.Designer.cs
+++ PegGui/PegGui/Startup.Designer.cs
@@ -31,7 +31,6 @@
             this.BtnEasy = new System.Windows.Forms.Button();
             this.BtnGod = new System.Windows.Forms.Button();
             this.BtnHard = new System.Windows.Forms.Button();
-            this.BtnMedium = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // BtnEasy
@@ -42,7 +41,7 @@
             this.BtnEasy.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.BtnEasy.Location = new System.Drawing.Point(22, 55);
             this.BtnEasy.Name = "BtnEasy";
-            this.BtnEasy.Size = new System.Drawing.Size(134, 42);
+            this.BtnEasy.Size = new System.Drawing.Size(161, 42);
             this.BtnEasy.TabIndex = 0;
             this.BtnEasy.Tag = "0";
             this.BtnEasy.Text = "Easy";
@@ -53,14 +52,14 @@
             // 
             this.BtnGod.BackColor = System.Drawing.Color.OrangeRed;
             this.BtnGod.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnGod.Font = new System.Drawing.Font("Microsoft Tai Le", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGod.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnGod.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.BtnGod.Location = new System.Drawing.Point(224, 148);
+            this.BtnGod.Location = new System.Drawing.Point(434, 55);
             this.BtnGod.Name = "BtnGod";
-            this.BtnGod.Size = new System.Drawing.Size(134, 42);
+            this.BtnGod.Size = new System.Drawing.Size(163, 42);
             this.BtnGod.TabIndex = 1;
-            this.BtnGod.Tag = "3";
-            this.BtnGod.Text = "God";
+            this.BtnGod.Tag = "2";
+            this.BtnGod.Text = "Impossible";
             this.BtnGod.UseVisualStyleBackColor = false;
             this.BtnGod.Click += new System.EventHandler(this.BtnEasy_Click);
             // 
@@ -68,39 +67,23 @@
             // 
             this.BtnHard.BackColor = System.Drawing.Color.OrangeRed;
             this.BtnHard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnHard.Font = new System.Drawing.Font("Microsoft Tai Le", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnHard.Font = new System.Drawing.Font("Microsoft Tai Le", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BtnHard.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.BtnHard.Location = new System.Drawing.Point(22, 148);
+            this.BtnHard.Location = new System.Drawing.Point(224, 55);
             this.BtnHard.Name = "BtnHard";
-            this.BtnHard.Size = new System.Drawing.Size(134, 42);
-            this.BtnHard.TabIndex = 2;
-            this.BtnHard.Tag = "2";
+            this.BtnHard.Size = new System.Drawing.Size(155, 42);
+            this.BtnHard.TabIndex = 3;
+            this.BtnHard.Tag = "1";
             this.BtnHard.Text = "Hard";
             this.BtnHard.UseVisualStyleBackColor = false;
             this.BtnHard.Click += new System.EventHandler(this.BtnEasy_Click);
-            // 
-            // BtnMedium
-            // 
-            this.BtnMedium.BackColor = System.Drawing.Color.OrangeRed;
-            this.BtnMedium.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.BtnMedium.Font = new System.Drawing.Font("Microsoft Tai Le", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BtnMedium.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.BtnMedium.Location = new System.Drawing.Point(224, 55);
-            this.BtnMedium.Name = "BtnMedium";
-            this.BtnMedium.Size = new System.Drawing.Size(134, 42);
-            this.BtnMedium.TabIndex = 3;
-            this.BtnMedium.Tag = "1";
-            this.BtnMedium.Text = "Medium";
-            this.BtnMedium.UseVisualStyleBackColor = false;
-            this.BtnMedium.Click += new System.EventHandler(this.BtnEasy_Click);
             // 
             // Battleship
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightCyan;
-            this.ClientSize = new System.Drawing.Size(386, 243);
-            this.Controls.Add(this.BtnMedium);
+            this.ClientSize = new System.Drawing.Size(630, 170);
             this.Controls.Add(this.BtnHard);
             this.Controls.Add(this.BtnGod);
             this.Controls.Add(this.BtnEasy);
@@ -120,6 +103,5 @@
         private System.Windows.Forms.Button BtnEasy;
         private System.Windows.Forms.Button BtnGod;
         private System.Windows.Forms.Button BtnHard;
-        private System.Windows.Forms.Button BtnMedium;
     }
 }
